bc0f1eb8c98aff600c5baf5e59972c28a23733f0
Luis Pladano <pladanoc@icloud.com>
2025-10-30 12:55:43 -0500

    fix: correct status handling in Letter and Window components

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,8 +1,8 @@
 export default function Letter({ txt, status }) {
   const statusColor = {
-    pending: "bg-[#63d0ff2c]",
-    incorrect: "bg-[#ff63632c]",
-    correct: "bg-[#63ffb92c]",
+    pending: "bg-[#63d0ff]",
+    incorrect: "bg-[#ff6363]",
+    correct: "bg-[#63ff]",
   };
 
   return <p className={statusColor[status]}>{txt}</p>;
--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -17,16 +17,28 @@ export default function Window({ text }) {
 
   function handleChange(e) {
     const input = e.key;
-    if (input === target[index].char) {
+    console.log(input);
+    if (input === target[index].char && index === target[index].index) {
       setTarget((prev) =>
         prev.map((item) =>
           item.index === index ? { ...item, status: "correct" } : item
         )
       );
       setIndex(index + 1);
+    } else if (input == "Backspace") {
+      setTarget((prev) =>
+        prev.map((item) =>
+          item.index === index ? { ...item, status: "pending" } : item
+        )
+      );
+      setIndex(index - 1);
     } else {
-      console.log(target[index]);
-      console.log("false");
+      setTarget((prev) =>
+        prev.map((item) =>
+          item.index === index ? { ...item, status: "incorrect" } : item
+        )
+      );
+      setIndex(index + 1);
     }
   }
 
