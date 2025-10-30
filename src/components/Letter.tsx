735e99b7d417a0ce293d09a38bf2a031e7d273ea
Luis Pladano <pladanoc@icloud.com>
2025-10-30 12:07:08 -0500

    feat: enhance Letter component with status-based styling and refactor Window component logic

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,3 +1,9 @@
-export default function Letter(props) {
-  return <p>{props.txt}</p>;
+export default function Letter({ txt, status }) {
+  const statusColor = {
+    pending: "bg-[#63d0ff2c]",
+    incorrect: "bg-[#ff63632c]",
+    correct: "bg-[#63ffb92c]",
+  };
+
+  return <p className={statusColor[status]}>{txt}</p>;
 }
--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,25 +1,48 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Letter from "./Letter";
 
+type Char = {
+  char: string;
+  status: string;
+  index: number;
+};
+
 export default function Window({ text }) {
-  const [target, setTarget] = useState(text.split(""));
+  const [target, setTarget] = useState(
+    text.split("").map((letter: string, i: number) => {
+      return { char: letter, status: "pending", index: i };
+    })
+  );
   const [index, setIndex] = useState(0);
+
+  function handleChange(e) {
+    const input = e.key;
+    if (input === target[index].char) {
+      setTarget((prev) =>
+        prev.map((item) =>
+          item.index === index ? { ...item, status: "correct" } : item
+        )
+      );
+      setIndex(index + 1);
+    } else {
+      console.log(target[index]);
+      console.log("false");
+    }
+  }
 
   console.log(target);
 
-  function handleChange(e) {
-    const newValue = e.nativeEvent.data;
-    console.log(index);
-    console.log(newValue);
-    target[index] === newValue ? setIndex(index + 1) : null;
-    !newValue ? setIndex(index - 1) : null;
-  }
-  const word = target.map((char) => <Letter txt={char} />);
+  const word = target.map((item: Char) => (
+    <Letter status={item.status} index={item.index} txt={item.char} />
+  ));
   return (
     <>
-      <h1>{target}</h1>
+      <h1>hello</h1>
       <div className="flex flex-row">{word}</div>
-      <textarea className="h-10 bg-[#63ffcb2c] absolute top-108" />
+      <textarea
+        onKeyDown={handleChange}
+        className="h-10 bg-[#63ffe2b0] absolute top-120"
+      />
     </>
   );
 }
